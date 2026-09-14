102abcbe53b82e0017b4b0d176e67dae18ca0dfb
tcp-maker <tcp@email.org>
2026-09-14 11:17:31 +0200

    Update GameContext to import Team type from types/teams

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-import { Team } from '../data/teams';
+import { Team } from '../types/teams';
 
 export interface Player {
   id: string;
